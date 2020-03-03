600763248dea1d10895ff06e8a28a461dcde88af
Farhan Shahbaz <49499937+Farhan12345a@users.noreply.github.com>
2020-03-02 22:05:04 -0500

    Add files via upload

---
+++ participation_07.c
@@ -0,0 +1,36 @@
+//Farhan Shahbaz
+
+//Welch
+
+
+
+#include <stdio.h>
+
+
+
+int main()
+{
+
+    char word[1000];
+
+    printf("Please enter a word for input\n");
+
+    fgets(word,1000,stdin);
+
+
+
+    printf("The uppercase word is:\n");
+
+    for(int i = 0; i < strlen(word); i++){
+ 
+       char a = word[i];
+
+       printf("%c", toupper(a));
+
+    }
+
+    
+	
+    return 0;
+
+}
